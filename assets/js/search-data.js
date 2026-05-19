598710aca58e73b0f996e27f27e87fca61586001
pragyankadel <59860119+pragyankadel@users.noreply.github.com>
2026-05-19 05:11:44 +0000

    Deploying to gh-pages from @ pragyankadel/pragyankadel.github.io@971e4a19d4740be962a00e19f65993c0ea0aaa2a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -177,34 +177,6 @@ ninja.data = [{
           window.open("mailto:%6B%61%64%65%6C%70%72%61%67%79%61%6E%33@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
